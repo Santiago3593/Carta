04b3e6ca58dfd35f8e6fab08bdd3146d879fdb7d
Santiago <dami.santi.f.hdz@gmail.com>
2026-07-05 17:02:32 -0600

    Fistr

---
+++ script.js
@@ -0,0 +1,23 @@
+const Carta1Dialog = document.getElementById("carta1-dialog")
+document.getElementById("carta1id").addEventListener("click", () => {
+  Carta1Dialog.showModal()
+})
+document.getElementById("cerrar-carta1").addEventListener("click", () => {
+  Carta1Dialog.close()
+})
+
+const Carta2Dialog = document.getElementById("carta2-dialog")
+document.getElementById("carta2id").addEventListener("click", () => {
+  Carta2Dialog.showModal()
+})
+document.getElementById("cerrar-carta2").addEventListener("click", () => {
+  Carta2Dialog.close()
+})
+
+const Carta3Dialog = document.getElementById("carta3-dialog")
+document.getElementById("carta3id").addEventListener("click", () => {
+  Carta3Dialog.showModal()
+})
+document.getElementById("cerrar-carta3").addEventListener("click", () => {
+  Carta3Dialog.close()
+})
